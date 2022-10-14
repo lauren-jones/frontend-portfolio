d45b5d2bb4a2c2f3537b1d5cff8269613d08dd81
lauren-jones <105777927+lauren-jones@users.noreply.github.com>
2022-10-14 20:11:47 +0100

    Added about component and styling

---
+++ src/components/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./about.css";
+
+export default function About() {
+  return (
+    <section className="About">
+      <img src="/images/image.png" alt="" />
+      <div className="About-details">
+        <h4>frontend dev</h4>
+        <h2>Portfolio</h2>
+        <p>
+          The most user-friendly and creative website platform you could ever
+          ask for! It makes it so simple to update the design, copy and images.
+          The most user-friendly and creative website platform you could ever
+          ask for! It makes it so simple to update the design, copy and images.
+        </p>
+      </div>
+    </section>
+  );
+}
---
+++ src/components/Ribbon.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./ribbon.css";
+
+export default function Ribbon() {
+  return (
+    <div className="Ribbon">
+      <ul>
+        <li>React</li>
+        <li>Typescript</li>
+        <li>Next JS</li>
+        <li>Node.js</li>
+        <li>Graph QL</li>
+        <li>CSS Grid</li>
+        <li>Flex Box</li>
+      </ul>
+    </div>
+  );
+}
